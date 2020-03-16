6b28145fff52fa3eacf16158953fe6ef4fc342ff
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-16 19:35:48 +0300

    mongoose initial setup

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
+
+// Connect to database
+connectDB();
 
 // Route files
 const restaurants = require('./routes/restaurants');
@@ -20,6 +25,14 @@ app.use('/api/v1/restaurants', restaurants);
   
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+});
+
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
 });
