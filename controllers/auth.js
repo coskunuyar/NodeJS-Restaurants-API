2ea2001b9c99c2054818761d952cc557480e6aa0
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-21 01:20:25 +0300

    logout controller and route

--- controllers/auth.js
+++ controllers/auth.js
@@ -154,3 +154,18 @@ exports.forgotPassword = asyncHandler(async (req,res,next) => {
 
     res.status(200).json({ success: true });
 });
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
